573761cc883a89565957e42d02257c9720432316
niamul007 <am3994566@gmail.com>
2025-11-22 13:56:45 +0600

    adding all src

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
-
+import UserProfile from './componenets/UserProfile.jsx'
 function App() {
 
   return (
     <>
-
+    <UserProfile/>
     </>
   )
 }
