7269072c9ccd24d7533e8b13704b7876b3c1510d
niamul007 <am3994566@gmail.com>
2025-11-22 19:57:04 +0600

    adding project 1

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import './App.css'
-import UserProfile from './componenets/UserProfile.jsx'
+import React from "react";
+import "./App.css";
+import UserProfile from "./componenets/UserProfile.jsx";
+import { ProfileCard } from "./project-1/ProfileCard.jsx";
 function App() {
+  // App.jsx
+  const user = {
+    name: "Niamul",
+    age: 22,
+    profession: "Learner",
+  };
+  const item = <ProfileCard name={user.name} profession={user.profession} age={user.age} />;
+
+  // render ProfileCard
 
   return (
     <>
-    <UserProfile/>
+    {item}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
