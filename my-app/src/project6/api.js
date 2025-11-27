1cc178b44a05eb31a49b3c0446dd66b057dc17fb
niamul007 <am3994566@gmail.com>
2025-11-27 22:07:59 +0600

    adding some practices

--- my-app/src/project6/api.js
+++ my-app/src/project6/api.js
@@ -6,7 +6,20 @@ const data = async () => {
   const data = await res.json();
   return console.log(data);
 };
-console.log(data());
+// console.log(data());
+
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// console.log(reverseString("Hello"));'
+
+
+function revers(str){
+  return str.split("").reverse().join("");
+}
+console.log(revers("Niamul"));
+// what join does is it joins the array elements into a string with the specified separator.
 
 
 export default data;
