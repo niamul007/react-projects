2b85e19bed1b8852dd9299242323e0915710b5ba
niamul007 <am3994566@gmail.com>
2025-11-27 21:52:05 +0600

    adding new changes here

--- my-app/src/project6/api.js
+++ my-app/src/project6/api.js
@@ -6,15 +6,7 @@ const data = async () => {
   const data = await res.json();
   return console.log(data);
 };
+console.log(data());
 
-const newData = data();
-const arr = newData.map((item) => ({
-  name: item.name.common,
-  capital: item.capital[0], 
-    region: item.region,
-    population: item.population,
-    flag: item.flags.png,
-}));    
 
-console.log(arr)
 export default data;
