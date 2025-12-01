2ce7b62b885428399283e23f53977e8fc43db84b
niamul007 <am3994566@gmail.com>
2025-12-01 20:08:08 +0600

    adding 2nd day chalange

--- my-app/src/dataProcess/dataProcessing.mjs
+++ my-app/src/dataProcess/dataProcessing.mjs
@@ -22,21 +22,50 @@ import RAW_FORECAST_DATA from "./data.js";
 // console.log(temparature.slice(0,5))
 
 const data = RAW_FORECAST_DATA.list;
-
-function kelvinToCelsius(kelvin){
-    kelvin = kelvin - 273.15 ;
-    return kelvin.toFixed(1)+ "°C"
+const uniqueDates = new Set();
+const processData = [];
+function kelvinToCelsius(kelvin) {
+  kelvin = kelvin - 273.15;
+  return kelvin.toFixed(1) + "°C";
 }
 
-const temparature = data.map((item)=>{
-    const date = item.dt_txt;
-    const temp = item.main.temp;
-    const description = item.weather[0].description;
-    return{
-        date,
-        temp,
-        description
-    }
+const temparature = data.map((item) => {
+  const dateAndTime = item.dt_txt;
+  const temp = kelvinToCelsius(item.main.temp);
+  const description = item.weather[0].description;
+  const dateKey = item.dt_txt.slice(0,10);
+  if(!uniqueDates.has(dateKey)){
+    uniqueDates.add(dateKey)
+    processData.push({
+        date: dateKey,
+        temp:temp,
+        description :description
+    })
+  }
+  return {
+    dateAndTime,
+    temp,
+    description,
+  };
 });
 
-console.log(temparature.slice(0,5));
+console.log(temparature.slice(0, 5));
+
+
+// const uniqueDates = new Set();
+// const processData = [];
+
+// data.forEach((item) => {
+//   const dateKey = item.dt_txt.slice(0, 10);
+//   if (!uniqueDates.has(dateKey)) {
+//     uniqueDates.add(dateKey);
+//     processData.push({
+//       date: dateKey,
+//       temp: item.main.temp,
+//       description: item.weather[0].description,
+//     });
+//   }
+// });
+
+console.log("Total unique dates found:", uniqueDates.size);
+console.log("Processed Data (First 5 Unique Entries):", processData.slice(0, 5));
