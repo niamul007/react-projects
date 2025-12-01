d51695c11332a498df632c653d7fc067328f1e4c
niamul007 <am3994566@gmail.com>
2025-12-01 19:17:50 +0600

    practincing details sesteructed

--- my-app/src/dataProcess/dataProcessing.mjs
+++ my-app/src/dataProcess/dataProcessing.mjs
@@ -1,22 +1,42 @@
 import RAW_FORECAST_DATA from "./data.js";
 
+// const data = RAW_FORECAST_DATA.list;
+
+// function kelvinToCelsius(kelvin){
+//     kelvin = kelvin- 273.15;
+//     return kelvin.toFixed(1)+"°C"
+// }
+// // console.log(kelvinToCelsius(290))
+
+// const temparature = data.map((item)=>{
+//     const date = item.dt_txt;
+//     const temp = kelvinToCelsius(item.main.temp);
+//     const iconCode = item.weather[0].icon;
+//     return {
+//         date,
+//         temp,
+//         iconCode
+//     }
+// })
+// console.log(temparature.length);
+// console.log(temparature.slice(0,5))
+
 const data = RAW_FORECAST_DATA.list;
 
 function kelvinToCelsius(kelvin){
-    kelvin = kelvin- 273.15;
-    return kelvin.toFixed(1)+"°C"
+    kelvin = kelvin - 273.15 ;
+    return kelvin.toFixed(1)+ "°C"
 }
-// console.log(kelvinToCelsius(290))
 
 const temparature = data.map((item)=>{
     const date = item.dt_txt;
-    const temp = kelvinToCelsius(item.main.temp);
-    const iconCode = item.weather[0].icon;
-    return {
+    const temp = item.main.temp;
+    const description = item.weather[0].description;
+    return{
         date,
         temp,
-        iconCode
+        description
     }
-})
-console.log(temparature.length);
-console.log(temparature.slice(0,5))
+});
+
+console.log(temparature.slice(0,5));
