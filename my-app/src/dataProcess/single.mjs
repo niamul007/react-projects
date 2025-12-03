c3b9c27010d90d3266c17efb8eb4b6075d536d79
niamul007 <am3994566@gmail.com>
2025-12-03 13:03:04 +0600

    adding new data for single funtion practice

---
+++ my-app/src/dataProcess/single.mjs
@@ -0,0 +1,17 @@
+const generateEngineeringAlphaAverage = (data) => {
+    
+    // --- Your chained solution goes here ---
+    // Start with the filters:
+    const alphaEngineers = data
+        // Filter 1: Department
+        // Filter 2: Activity
+        // Filter 3: Project (Nesting)
+        
+        // Map: Isolate Salary
+        
+        // Reduce: Sum the salaries
+
+    // Calculate the final average here (Sum / Count)
+    
+    // return ...
+};
