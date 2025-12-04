ccf89fb0fb9fb9b52f08a6c062cb2bab285546a8
niamul007 <am3994566@gmail.com>
2025-12-04 21:50:26 +0600

    adding single function work

--- my-app/src/dataProcess/single.mjs
+++ my-app/src/dataProcess/single.mjs
@@ -1,17 +1,26 @@
+import EMPLOYEE_DATA from "./singleData.js";
+
+
+let active = true;
 const generateEngineeringAlphaAverage = (data) => {
     
     // --- Your chained solution goes here ---
     // Start with the filters:
-    const alphaEngineers = data
-        // Filter 1: Department
+    const alphaEngineers = data;
+    const department = alphaEngineers.filter(item=> item.department === "Engineering")
+    // console.log(department)
         // Filter 2: Activity
+    const activeWorker = alphaEngineers.filter(item => item.isActive === active);
+    console.log(activeWorker)
         // Filter 3: Project (Nesting)
-        
+        const alpha = data.filter(item => item.projects.includes("Alpha"))
+        console.log(alpha)
         // Map: Isolate Salary
         
         // Reduce: Sum the salaries
 
     // Calculate the final average here (Sum / Count)
     
-    // return ...
+    return;
 };
+generateEngineeringAlphaAverage(EMPLOYEE_DATA)
